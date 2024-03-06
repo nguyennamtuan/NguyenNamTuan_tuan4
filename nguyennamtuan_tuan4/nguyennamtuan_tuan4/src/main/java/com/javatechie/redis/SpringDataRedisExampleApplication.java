834bf0e615f5fc636c48c37ac90b15d90a99ab93
nguyennamtuan <100201031+nguyennamtuan@users.noreply.github.com>
2024-03-06 09:27:53 +0700

    Add files via upload

---
+++ nguyennamtuan_tuan4/nguyennamtuan_tuan4/src/main/java/com/javatechie/redis/SpringDataRedisExampleApplication.java
@@ -0,0 +1,46 @@
+package com.javatechie.redis;
+
+import com.javatechie.redis.entity.Product;
+import com.javatechie.redis.respository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/product")
+public class SpringDataRedisExampleApplication {
+	public static final String HASH_KEY = "Product";
+    @Autowired
+    private RedisTemplate template;
+    private ProductDao dao;
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+    @DeleteMapping("/{id}")
+    public String remove(@PathVariable int id)   {
+    	return dao.deleteProduct(id);
+	}
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRedisExampleApplication.class, args);
+    }
+
+}
